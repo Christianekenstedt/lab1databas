821a65ba3bce94449294ff7ee012c0ada981d6d3
chris <chris@192.168.0.37>
2015-12-03 19:50:24 +0100

    Now started to convert from database to objects

--- src/lab1databas/FXMLMainViewController.java
+++ src/lab1databas/FXMLMainViewController.java
@@ -16,6 +16,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ObservableValue;
@@ -142,43 +144,54 @@ public class FXMLMainViewController implements Initializable {
         return connectToDB(user,pwd);
     }
     public static void executeQuery(Connection con, String query) throws SQLException {
-		Statement stmt = null;
-	    try {
-	    	// Execute the SQL statement
-	    	stmt = con.createStatement();
-	    	ResultSet rs = stmt.executeQuery(query);
-	    	
-	    	// Get the attribute names
-	    	ResultSetMetaData metaData = rs.getMetaData();
-	    	int ccount = metaData.getColumnCount();
-                
-	    	for(int c = 1; c <= ccount; c++) {
-                        
-	    		System.out.print(metaData.getColumnName(c) + "\t");
-                        
-	    	}
-	    	System.out.println();
-	    	// Get the attribute values
-	    	while (rs.next()) {
-    			// NB! This is an example, -not- the preferred way to retrieve data.
-    			// You should use methods that return a specific data type, like
-    			// rs.getInt(), rs.getString() or such.
-    			// It's also advisable to store each tuple (row) in an object of
-    			// custom type (e.g. Employee).
-	    		for(int c = 1; c <= ccount; c++) {
-                                
-	    			System.out.print(rs.getObject(c) + "\t");
-	    		}
-		        System.out.println();
-	    	}
-	    	
-	    }	
-	    finally {
-	    	if (stmt != null) { 
-	    		stmt.close(); 
-	    	}
-	    }
-	}
+            new Thread(){
+                @Override
+                public void run(){
+                    Statement stmt = null;
+                    try {
+                        // Execute the SQL statement
+                        stmt = con.createStatement();
+                        ResultSet rs = stmt.executeQuery(query);
+
+                        // Get the attribute names
+                        ResultSetMetaData metaData = rs.getMetaData();
+                        int ccount = metaData.getColumnCount();
+
+                        for(int c = 1; c <= ccount; c++) {
+
+                                System.out.print(metaData.getColumnName(c) + "\t");
+
+                        }
+                        System.out.println();
+                        // Get the attribute values
+                        while (rs.next()) {
+                                // NB! This is an example, -not- the preferred way to retrieve data.
+                                // You should use methods that return a specific data type, like
+                                // rs.getInt(), rs.getString() or such.
+                                // It's also advisable to store each tuple (row) in an object of
+                                // custom type (e.g. Employee).
+                                for(int c = 1; c <= ccount; c++) {
+
+                                        System.out.print(rs.getObject(c) + "\t");
+                                }
+                                System.out.println();
+                        }
+
+                    }	
+                    catch (SQLException ex) {
+                        Logger.getLogger(FXMLMainViewController.class.getName()).log(Level.SEVERE, null, ex);
+                    }                    finally {
+                        if (stmt != null) { 
+                            try { 
+                                stmt.close();
+                            } catch (SQLException ex) {
+                                Logger.getLogger(FXMLMainViewController.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                        }
+                    }
+                }
+            }.start();
+    }
     // TEST MED PREPARED STATEMENTS OCH ATT GÖRA OM OBJEKT FRÅN DBn.
     public ArrayList<Artist> getArtistByName(String name) throws SQLException{
         ResultSet rs = null;
