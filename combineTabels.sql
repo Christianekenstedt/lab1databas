de851bec61ebe452554c9b2ef427891e2d473e11
chris <chris@192.168.0.37>
2015-12-04 00:19:19 +0100

    Fixed stuff

--- combineTabels.sql
+++ combineTabels.sql
@@ -2,3 +2,5 @@
 
 
 SELECT Album.name, Artist.name, Album.releaseDate FROM Album, Album_Artist, Artist where Album.albumID = Album_Artist.album and Album_Artist.artist = Artist.artistID;
+
+SELECT Album.name, Artist.name FROM Album, Album_Artist, Artist WHERE Album.albumID = Album_Artist.album and Album_Artist.artist = Artist.artistID AND Artist.name = 'Pink Floyd';
