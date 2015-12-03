60f9ace9d9a05612f92030e71474fd28434e601d
chris <chris@192.168.0.37>
2015-12-03 14:18:30 +0100

    Now started to convert from database to objects

--- src/lab1databas/FXMLMainViewController.java
+++ src/lab1databas/FXMLMainViewController.java
@@ -203,49 +203,31 @@ public class FXMLMainViewController implements Initializable {
           
           data = FXCollections.observableArrayList();
           try{
-            
-            //SQL FOR SELECTING ALL OF CUSTOMER
             String SQL = "SELECT * from "+tableType;
-            //String SQL = "SELECT Album.name, Artist.name, Album.releaseDate FROM Album, Album_Artist, Artist where Album.albumID = Album_Artist.album and Album_Artist.artist = Artist.artistID";
-            //ResultSet
+            
             ResultSet rs = con.createStatement().executeQuery(SQL);
-
-            /**********************************
-             * TABLE COLUMN ADDED DYNAMICALLY *
-             **********************************/
-            for(int i=0 ; i<rs.getMetaData().getColumnCount(); i++){
+            for(int i=0 ; i<rs.getMetaData().getColumnCount(); i++){ // Gets the number of columns dynamically
                 //We are using non property style for making dynamic table
-                final int j = i;                
+                int j = i;                
                 TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i+1));
                 col.setCellValueFactory(new Callback<CellDataFeatures<ObservableList,String>,ObservableValue<String>>(){                    
                     public ObservableValue<String> call(CellDataFeatures<ObservableList, String> param) {                                                                                              
                         return new SimpleStringProperty(param.getValue().get(j).toString());                        
                     }                    
                 });
-               
                 table.getColumns().addAll(col); 
                 System.out.println("Column ["+i+"] ");
             }
 
-            /********************************
-             * Data added to ObservableList *
-             ********************************/
             while(rs.next()){
-                //Iterate Row
                 ObservableList<String> row = FXCollections.observableArrayList();
                 for(int i=1 ; i<=rs.getMetaData().getColumnCount(); i++){
-                    //Iterate Column
                     row.add(rs.getString(i));
-                    
                 }
                 System.out.println("Row added "+row );
                 data.add(row);
-
             }
-
-            //FINALLY ADDED TO TableView
             table.setItems(data);
-            
           }catch(Exception e){
               e.printStackTrace();
               System.out.println("Error on Building Data");             
@@ -280,7 +262,9 @@ public class FXMLMainViewController implements Initializable {
         }
     }
     
-    
+    private void presentTableWithList(ArrayList<Object> list){
+        // VET EJ :O
+    }
     
     
 }
