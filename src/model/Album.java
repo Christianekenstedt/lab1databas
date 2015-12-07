d554cfc544690d27c56abd4ad58cdc3cedee0980
chris <chris@192.168.0.37>
2015-12-07 19:03:42 +0100

    fixed javadocs

--- src/model/Album.java
+++ src/model/Album.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
 import java.io.Serializable;
-import java.util.Collection;
 import java.util.Date;
-import javax.xml.bind.annotation.XmlTransient;
 
 /**
  * Album contains methods to access the result from the database
